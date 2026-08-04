7477766362251d6a33943be184382a671774db44
satish <satish@gmail.com>
2026-08-04 22:46:50 +0530

    updated file content

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,6 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
-  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
