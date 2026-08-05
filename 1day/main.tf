5141400ead73cceed31c321ffee8fad3291015d8
satish <satish@gmail.com>
2026-08-05 20:08:15 +0530

    updated file content

--- 1day/main.tf
+++ 1day/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "s3-test357-dev"
+  bucket = "s3-test357-var.enviroment"
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
+    Environment = var.enviroment
   }
 }
